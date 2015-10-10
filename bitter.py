4ec908abaa15533601587f63da2caeafccdb7369
Sean <toranos@gmail.com>
2015-10-10 22:33:07 +1100

    Fix missing quotation marks and tweak search bar

--- bitter.py
+++ bitter.py
@@ -124,9 +124,9 @@ def page_header():
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 						<span class="sr-only">Toggle nagivation</span>
-						<span class="icon-bar></span>
-						<span class="icon-bar></span>	
-						<span class="icon-bar></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>	
+						<span class="icon-bar"></span>
 					</button>
 					<a class="navbar-brand" href="#">Bitter</a>
 				</div>
@@ -137,12 +137,14 @@ def page_header():
 						<li><a href="#">New Bleat</a></li>
 					</ul>
 					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">
-							<span class="glyphicon glyphicon-search" aria-hidden="true"</span>
-						</button>
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search">
+						<span class="input-group-btn">
+							<button type="submit" class="btn btn-default">
+								<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+							</button>
+						</span>
+					</div>
 					</form>
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="#">Log Out</a></li>
