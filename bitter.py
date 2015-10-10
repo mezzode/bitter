d73f6f160e956ab42393c4788aaf66aeaa653122
Sean <toranos@gmail.com>
2015-10-11 01:43:23 +1100

    Add page selector for bleats

--- bitter.py
+++ bitter.py
@@ -116,6 +116,25 @@ def user_page(parameters, users_dir, bleats_dir):
                     %s
                 </ul>
             </div>
+            <nav>
+              <ul class="pagination">
+                <li class="disabled">
+                  <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li>
+                  <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
         </div>
         <div class="col-md-3 col-sm-7">
             <div class="panel panel-primary">
