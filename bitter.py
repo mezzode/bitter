990aa7fd8044145d3340cca05f33882542c82cac
Sean <toranos@gmail.com>
2015-10-12 22:55:11 +1100

    Tweak search page sizes

--- bitter.py
+++ bitter.py
@@ -250,10 +250,10 @@ def search_page(parameters, users_dir, bleats_dir):
         results += """<button type="submit" form="main" name="user" value=%s class="list-group-item">
 <div class="media">
     <div class="media-left">
-        <img class="media-object" src="%s" height="128" width="128">
+        <img class="media-object" src="%s" height="100" width="100">
     </div>
     <div class="media-body">
-        <h2 class="media-heading">%s<br><small>%s</small></h4>
+        <h3 class="media-heading">%s<br><small>%s</small></h3>
     </div>
 </div>
 </button>
@@ -277,7 +277,7 @@ def search_page(parameters, users_dir, bleats_dir):
         <div class="col-md-12 col-sm-12">
             <div class="panel panel-primary">
                 <div class="panel-body">
-                    <h1 class="list-group-item-heading">Search Results: <small>%s</small></h1>
+                    <h2 class="list-group-item-heading">Search Results: <small>%s</small></h2>
                     <ul class="nav nav-pills">
                         <li role="presentation" class="active"><a href="#users" data-toggle="pill">Users</a></li>
                         <li role="presentation"><a href="#bleats" data-toggle="pill">Bleats</a></li>
