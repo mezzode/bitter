746dd2683927984ee15fa8ef84f153b2ac9502d1
Sean <toranos@gmail.com>
2015-10-24 01:09:38 +1100

    Add accordion to edit page

--- bitter.py
+++ bitter.py
@@ -149,6 +149,115 @@ def edit_details():
         <div class="col-md-3">
         </div>
         <div class="col-md-6 col-md-12">
+
+<div class="panel-group" id="edit">
+<div class="panel panel-default">
+<div class="list-group">
+<div class="list-group-item">
+<a style="color: inherit;" class="list-group-item-heading" href="#details" data-toggle="collapse" data-parent="#edit"><h4 class="list-group-item-heading">Details</h4></a>
+<!--<p class="lead">It's snowing again</p>-->
+</div>
+</div>
+<div class="collapse panel-collapse" id="details">
+    <ul class="list-group">
+    <li class="list-group-item">
+        <form method="POST" bleat>
+        <div class="form-group">
+        <textarea name="new-bleat" placeholder="Your reply" class="form-control" rows="3" maxlength="142"></textarea>
+        <span id="helpBlock" class="help-block pull-right">0/142</span>
+        </div>
+        <button type="submit" class="btn btn-default" disabled="disabled">Submit</button>
+        </form>
+    </li>
+    </ul>
+</div>
+</div>
+
+<div class="panel panel-default">
+<div class="list-group">
+<div class="list-group-item">
+<a style="color: inherit;" class="list-group-item-heading" href="#password" data-toggle="collapse" data-parent="#edit"><h4 class="list-group-item-heading">Password</h4></a>
+<!--<p class="lead">It's snowing again</p>-->
+</div>
+</div>
+<div class="collapse panel-collapse" id="password">
+    <ul class="list-group">
+    <li class="list-group-item">
+        <form method="POST" bleat>
+        <div class="form-group">
+        <textarea name="new-bleat" placeholder="Your reply" class="form-control" rows="3" maxlength="142"></textarea>
+        <span id="helpBlock" class="help-block pull-right">0/142</span>
+        </div>
+        <button type="submit" class="btn btn-default" disabled="disabled">Submit</button>
+        </form>
+    </li>
+    </ul>
+</div>
+</div>
+
+</div><!--panel group-->
+
+<!--<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<div class="panel panel-default">
+    <div class="list-group">
+    <div class="list-group-item">
+      <h4 style="color: inherit;">
+        <a role="button" class="list-group-item-heading" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </a>
+      </h4>
+    </div>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+        <ul class="list-group">    
+        <li class="list-group-item">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        </li>
+        </ul>
+    </div>
+</div>
+  
+  <div class="panel panel-default">
+    <div class="list-group">
+    <div class="list-group-item">
+    <div class="list-group-item-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </a>
+      </h4>
+    </div>
+    </div>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+       <ul class="list-group">    
+        <li class="list-group-item">
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+    </li>
+    </ul>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="list-group">
+    <div class="list-group-item">
+        <div class="list-group-item-heading" role="tab" id="headingThree">
+          <h4 class="panel-title">
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Collapsible Group Item #3
+            </a>
+          </h4>
+        </div>
+        </div>
+        </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+         <ul class="list-group">    
+        <li class="list-group-item">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </li>
+      </ul>
+    </div>
+  </div>
+</div>-->
             <h1>Edit Details</h1>
             <form method="POST">
                 <div class="form-group">
